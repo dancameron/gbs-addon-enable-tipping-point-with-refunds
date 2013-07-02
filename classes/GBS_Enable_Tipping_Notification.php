5c71a4ac15df8a93ba48fa7a616972f787aae817
Dan Cameron <dan@sproutventure.com>
2013-07-02 16:38:01 -0700

    Notification and application.

--- classes/GBS_Enable_Tipping_Notification.php
+++ classes/GBS_Enable_Tipping_Notification.php
@@ -1,7 +1,7 @@
 <?php
 
 class GBS_Enable_Tipping_Notification {
-	const NOTIFICATION_TYPE = 'free_deal_notification';
+	const NOTIFICATION_TYPE = 'refunded_user_notification';
 
 	public static function init() {
 		add_filter( 'gb_notification_types', array( get_class(), 'register_notification_type' ), 10, 1 );
@@ -10,22 +10,25 @@ class GBS_Enable_Tipping_Notification {
 
 	public function register_notification_type( $notifications ) {
 		$notifications[self::NOTIFICATION_TYPE] = array(
-			'name' => gb__( 'Free Deal Notification' ),
-			'description' => gb__( "Customize the notification sent to the customer after receiving a free deal." ),
-			'shortcodes' => array( 'date', 'name', 'username', 'deal_url', 'deal_title', 'site_title', 'site_url' ),
-			'default_title' => gb__( 'Free Deal at ' . get_bloginfo( 'name' ) ),
-			'default_content' => sprintf( 'You just received a free deal at %s.', get_bloginfo( 'name' ) ),
+			'name' => gb__( 'Payment Refunded Notification' ),
+			'description' => gb__( "Customize the notification sent to the customer after being refunded for a deal that failed." ),
+			'shortcodes' => array( 'date', 'name', 'username', 'deal_url', 'deal_title', 'site_title', 'site_url', 'reward' ),
+			'default_title' => gb__( 'Payment Refunded at ' . get_bloginfo( 'name' ) ),
+			'default_content' => sprintf( 'A payment of yours has been refunded for a failed item at %s.', get_bloginfo( 'name' ) ),
 			'allow_preference' => TRUE
 		);
 		return $notifications;
 	}
 
-	public function notification( $user_id, $deal_id ) {
+	public function notification( $user_id, $deal_id, $refund_amount, $credit_type ) {
 		$deal = Group_Buying_Deal::get_instance( $deal_id );
 		$to = Group_Buying_Notifications::get_user_email( $user_id );
 		$data = array(
 			'user_id' => $user_id,
-			'deal' => $deal
+			'deal' => $deal,
+			'applied_credits' => $refund_amount,
+			'type' => $credit_type
+
 		);
 		Group_Buying_Notifications::send_notification( self::NOTIFICATION_TYPE, $data, $to );
 	}
--- classes/GBS_Refund_Users.php
+++ classes/GBS_Refund_Users.php
@@ -8,7 +8,7 @@ class GBS_Refund_Users {
 
 	public function maybe_refund( Group_Buying_Deal $deal ) {
 		$item_id = $deal->get_id();
-		$refund = 0;
+		$refund_amount = 0;
 		// All purchases
 		$purchases = $deal->get_purchases();
 		foreach ( $purchases as $purchase ) {
@@ -17,26 +17,68 @@ class GBS_Refund_Users {
 			foreach ( $payments as $payment_id ) {
 				$payment = Group_Buying_Payment::get_instance( $payment_id );
 				$payment_method = $payment->get_payment_method();
-				// Don't handle credit purchases
+				// Don't handle credit payments
 				if ( $payment_method != Group_Buying_Account_Balance_Payments::PAYMENT_METHOD || $payment_method != Group_Buying_Affiliate_Credit_Payments::PAYMENT_METHOD ) {
+
 					$items = $payment->get_deals();
 					// Loop through the payments deals
-					foreach ( $items as $key_item_id => $purchase_item ) {
+					foreach ( $items as $deal_id => $purchase_items ) {
 						// Only those deals that have failed
-						if ( $item_id == $key_item_id ) {
+						if ( $item_id == $deal_id ) {
 							// loop through all items
-							foreach ( $purchase_item as $key => $value ) {
-								$refund += $items[$key_item_id][$key]['price'];
+							foreach ( $purchase_items as $item ) {
+								foreach ( $item['payment_method'] as $payment_method => $payment ) {
+									// again don't tally credits
+									if ( $payment_method != Group_Buying_Account_Balance_Payments::PAYMENT_METHOD || $payment_method != Group_Buying_Affiliate_Credit_Payments::PAYMENT_METHOD ) {
+										$refund_amount += $payment;
+									}
+								}
 							}
 						}
 					}
+
 				}
-				// Refund the user
-
-
-
 			}
+			// Refund the user
+			$account_id = $purchase->get_account_id();
+			$account = Group_Buying_Account::get_instance_by_id( $account_id );
+
+			// Add Credit
+			$credit_type = apply_filters('gbs_tipping_point_refund', GBS_Enable_Tipping_Addon::CREDIT_TYPE);
+			$account->add_credit( $refund_amount, $credit_type );
+
+			// Fire off the notification manually
+			GBS_Enable_Tipping_Notification::notification( $account->get_user_id(), $item_id, $refund_amount, $credit_type );
+
+			// Record reward
+			self::reward_applied_record( $account, $purchase->get_id(), $refund_amount, $credit_type );
+
+			// Reset for other purchases
+			$refund_amount = 0;
+			// continue loop of purchases
 		}
 	}
 
+	public static function reward_applied_record( $account, $purchase_id, $refund_amount, $credit_type ) {
+		$account_id = $account->get_ID();
+		$balance = $account->get_credit_balance( $type );
+
+		// Setup data array
+		$data = array();
+		$data['account_id'] = $account_id;
+		$data['purchase_id'] = $purchase_id;
+		$data['credits'] = $refund_amount;
+		$data['type'] = $type;
+		$data['current_total_'.$type] = $balance;
+		$data['change_'.$type] = $refund_amount;
+
+		// Record
+		Group_Buying_Records::new_record(
+			sprintf( gb__( 'Payment Refunded from Purchase #%s' ), $purchase_id ),
+			$credit_type,
+			sprintf( gb__( 'Payment Refunded from Purchase #%s' ), $purchase_id ),
+			1,
+			$account_id,
+			$data );
+	}
 }
